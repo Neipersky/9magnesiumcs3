233e4ae1e872b6f0af2332d7a4a78801e18dc9f1
ethan <edpsolano@brc.pshs.edu.ph>
2026-08-19 20:29:17 +0800

    Add files via upload

---
+++ q1/zodiacMagnesiumSolano.py
@@ -0,0 +1,22 @@
+birth_year = int(input("Enter your birth year: "))
+
+if birth_year < 1900:
+    print("Please enter a year from 1900 onwards.")
+else:
+    zodiac = [
+        "Rat (鼠 / Shǔ)",
+        "Ox (牛 / Niú)",
+        "Tiger (虎 / Hǔ)",
+        "Rabbit (兔 / Tù)",
+        "Dragon (龙 / Lóng)",
+        "Snake (蛇 / Shé)",
+        "Horse (马 / Mǎ)",
+        "Goat (羊 / Yáng)",
+        "Monkey (猴 / Hóu)",
+        "Rooster (鸡 / Jī)",
+        "Dog (狗 / Gǒu)",
+        "Pig (猪 / Zhū)"
+    ]
+
+    number = (birth_year - 1900) % 12
+    print("Your Chinese Zodiac Sign is:", zodiac[number])
